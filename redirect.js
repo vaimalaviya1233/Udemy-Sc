efb75f410bd3c9b173525d47fcbf3b79fc3e1f9b
vaibhav malaviya <Vaibhavmalaviya@yahoo.com>
2021-08-16 16:36:18 -0500

    start

---
+++ redirect.js
@@ -0,0 +1,16 @@
+//gets url of the page and matches it 
+var url = window.location.href;//output is  https://icontricks.tech/
+var url_array = url.split("/");// output is ['https:', '', 'icontricks.tech', '']
+var basicurl = url_array[2];//output should be icontricks.tech;
+// console.log(basicurl);
+var absolutesite = ["https://icontricks.tech/", "https://coursevania.com", "https://getintocourse.com/"];
+var sites = ["icontricks.tech", "coursevania.com", "getintocourse.com", "udemy.com"];
+
+for (var i = 0; i < sites.length; i++) {
+    if (sites[i] == basicurl) {
+        reorder(sites[i]);
+    }
+}
+// var url_array_length = url_array.length;
+// var url_array_length_minus_one = url_array_length - 1;
+// var url_array_length_minus_two = url_array_length - 2;
---
+++ reorder.js
@@ -0,0 +1,88 @@
+function reorder(url) {
+    //matches url with website address
+    switch (url) {
+        case "getintocourse.com":
+            getintocourse();
+            break;
+        case "coursevania.com":
+            coursevania();
+            break;
+        case "icontricks.tech":
+            icontricks();
+            break;
+        case "udemy.com":
+            udemy();
+            break;
+        default:
+            break;
+    }
+}
+function getintocourse() {
+    //getintocourse.com
+    let re_track_btn = document.getElementsByClassName('re_track_btn');
+    let blank = document.getElementsByClassName('_blank');
+    let free_course = document.getElementsByClassName('maxbutton-get-this-free-course');
+    let button = document.getElementsByClassName('wp-block-button__link');
+    let readmore = document.getElementsByClassName("readmore");
+
+    if (re_track_btn.length) {
+        window.location.assign(document.getElementsByClassName('re_track_btn')[0]["href"]);
+        console.log(" found re_track_btn");
+    } else if (blank.length) {
+        window.location.assign(document.getElementsByClassName('_blank')[0]["href"]);
+        console.log("found blank");
+    } else if (free_course.length) {
+        window.location.assign(document.getElementsByClassName('maxbutton-get-this-free-course')[0]["href"]);
+        console.log("found free_course");
+    } else if (button.length) {
+        window.location.assign(document.getElementsByClassName('wp-block-button__link')[0]["href"]);
+        console.log("found button");
+    } else if (readmore.length) {
+        window.location.assign(document.getElementsByClassName("readmore")[0]["children"][0]["href"]);
+        console.log("found readmore");
+    } else {
+        alert('non exist');
+        console.log("nothing found");
+    }
+}
+
+function udemy() {
+    wait(3000);
+    var butspan = document.querySelector("#udemy > div.main-content-wrapper > div.main-content > div.paid-course-landing-page__container > div.sidebar-container-position-manager > div > div > div > div.course-landing-page_sidebar-container.sidebar-container--fixed--2xu7a > div > div:nth-child(1) > div.sidebar-container--purchase-section--17KRp > div > div.generic-purchase-section--buy-box-main--siIXV > div > div.buy-button.buy-box--buy-box-item--1Qbkl.buy-box--buy-button--1mpz_ > div > button > span");
+    var button = document.querySelector("#udemy > div.main-content-wrapper > div.main-content > div.paid-course-landing-page__container > div.sidebar-container-position-manager > div > div > div > div.course-landing-page_sidebar-container.sidebar-container--fixed--2xu7a > div > div:nth-child(1) > div.sidebar-container--purchase-section--17KRp > div > div.generic-purchase-section--buy-box-main--siIXV > div > div.buy-button.buy-box--buy-box-item--1Qbkl.buy-box--buy-button--1mpz_ > div > button");
+    if ((butspan["innerHTML"] == "Enroll now")) {
+        document.querySelector("#udemy > div.main-content-wrapper > div.main-content > div.paid-course-landing-page__container > div.sidebar-container-position-manager > div > div > div > div.course-landing-page_sidebar-container.sidebar-container--fixed--2xu7a > div > div:nth-child(1) > div.sidebar-container--purchase-section--17KRp > div > div.generic-purchase-section--buy-box-main--siIXV > div > div.buy-button.buy-box--buy-box-item--1Qbkl.buy-box--buy-button--1mpz_ > div > button").click();
+    }
+    else if (butspan["innerHTML"] == "Go to course") {
+        window.close();
+    }
+}
+
+function coursevania() {
+    //coursevania.com
+    window.location.assign(document.getElementsByClassName('no-price')[0]["href"]);
+}
+
+function icontricks() {
+    //icontricks.tech
+    //checks if element exists by document.querySelector
+    function elementExists(selector) {
+        return document.querySelector(selector) !== null;
+    }
+
+    function getLink(el) {
+        var link = el.getAttribute("href");
+        return link;
+    }
+
+    var selectors = ["#the-post > div.entry-content.entry.clearfix > center > button > a", "#the-post > div.entry-content.entry.clearfix > center > button > a", "#the-post > div.entry-content.entry.clearfix > center > button > a", "#the-post > div.entry-content.entry.clearfix > center > button > a"];
+
+    for (var i = 0; i < selectors.length; i++) {
+        if (elementExists(selectors[i])) {
+            console.log(selectors[i]);
+            window.location.assign(document.querySelector(selectors[i])["href"]);
+        }
+    }
+}
+
+
