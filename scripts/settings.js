ce3fc62ba4d41fe561134983c378b159fa242c62
vaimalaviya1233 <vaibhavmalaviya@yahoo.com>
2022-04-07 20:31:31 -0500

    updated settings ui

---
+++ scripts/settings.js
@@ -0,0 +1,38 @@
+// Initialize butotn with users's prefered color
+/* let changeColor = document.getElementById("changeColor");
+chrome.storage.sync.get("color", ({ color }) => {
+  changeColor.style.backgroundColor = color;
+}); */
+// When the button is clicked, inject setPageBackgroundColor into current page
+/* changeColor.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: setPageBackgroundColor,
+  });
+}); */
+// The body of this function will be execuetd as a content script inside the
+// current page
+/* function setPageBackgroundColor() {
+  chrome.storage.sync.get("color", ({ color }) => {
+    document.body.style.backgroundColor = color;
+  });
+}
+ */
+/* document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(elems, options);
+}); */
+/* import("./scripts/materialize.js");
+
+const M = require('scripts/materialize.js');
+
+//var instance = M.Collapsible.getInstance(elem);
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(elems, 'inDuration');
+  instances.open(0);
+
+}); */
+
